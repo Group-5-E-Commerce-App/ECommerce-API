bda2f7936e9c56b1867c666f155985f531456321
Rischiyuda <rischiyuda17@gmail.com>
2023-01-23 21:45:55 +0700

    features user

---
+++ features/user/data/model.go
@@ -0,0 +1,52 @@
+package data
+
+import (
+	"ecommerce/features/user"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name     string
+	Email    string
+	Username string
+	Password string
+	// Product  []data.Product `gorm:"foreignKey:UserID;references:ID;constraint:OnDelete:CASCADE"`
+}
+
+func ToCore(data User) user.Core {
+	return user.Core{
+		ID:       data.ID,
+		Name:     data.Name,
+		Email:    data.Email,
+		Username: data.Username,
+		Password: data.Password,
+	}
+}
+
+func (dataModel *User) ModelsToCore() user.Core {
+	return user.Core{
+		ID:       dataModel.ID,
+		Name:     dataModel.Name,
+		Email:    dataModel.Email,
+		Username: dataModel.Username,
+	}
+}
+func listModelToCore(dataModel []User) []user.Core {
+	var dataCore []user.Core
+	for _, v := range dataModel {
+		dataCore = append(dataCore, v.ModelsToCore())
+	}
+	return dataCore
+}
+
+func CoreToData(data user.Core) User {
+	return User{
+		Model:    gorm.Model{ID: data.ID},
+		Name:     data.Name,
+		Email:    data.Email,
+		Username: data.Username,
+		Password: data.Password,
+	}
+}
---
+++ features/user/handler/response.go
@@ -0,0 +1,38 @@
+package handler
+
+import "ecommerce/features/user"
+
+type UserReponse struct {
+	ID       uint   `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func ToResponse(data user.Core) UserReponse {
+	return UserReponse{
+		ID:       data.ID,
+		Name:     data.Name,
+		Email:    data.Email,
+		Username: data.Username,
+		Password: data.Password,
+	}
+}
+
+func ToResponses(data user.Core) UserReponse {
+	return UserReponse{
+
+		Name:     data.Name,
+		Email:    data.Email,
+		Username: data.Username,
+	}
+}
+func fromCoreList(dataCore []user.Core) []UserReponse {
+	var dataResponse []UserReponse
+
+	for _, v := range dataCore {
+		dataResponse = append(dataResponse, ToResponse(v))
+	}
+	return dataResponse
+}
